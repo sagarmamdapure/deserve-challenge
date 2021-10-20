dae9c400351c6708a6bdc7284ede693ba4d55427
Sagar Mamdapure <sagarmamdapure@gmail.com>
2021-10-20 12:32:13 +0530

    minor fix

--- src/main/java/org/deserve/challenge/Board.java
+++ src/main/java/org/deserve/challenge/Board.java
@@ -4,16 +4,18 @@ import java.util.Map;
 
 public class Board {
     private final Map<Integer, Integer> snakes;
+    private final int maxPosition;
     private int currentPosition;
 
     public Board(int startPosition, int maxPosition, Map<Integer, Integer> snakes) {
         this.snakes = snakes;
         this.currentPosition = startPosition;
+        this.maxPosition = maxPosition;
     }
 
     public int play(int faceValue) {
         this.currentPosition += faceValue;
-        if (this.currentPosition >= 100) {
+        if (this.currentPosition >= maxPosition) {
             System.out.printf("Roll dice face value is %d, current position is %d. GAME OVER", faceValue, this.currentPosition);
             return -1;
         }
